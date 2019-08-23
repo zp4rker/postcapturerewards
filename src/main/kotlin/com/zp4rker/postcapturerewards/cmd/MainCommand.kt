482b59aededa34d8836df9dab8e66d8f26257066
zp4rker <me@zp4rker.com>
2019-08-23 16:43:35 +1000

    implemented create sub command

--- src/main/kotlin/com/zp4rker/postcapturerewards/cmd/MainCommand.kt
+++ src/main/kotlin/com/zp4rker/postcapturerewards/cmd/MainCommand.kt
@@ -1,11 +1,23 @@
 package com.zp4rker.postcapturerewards.cmd
 
+import com.zp4rker.postcapturerewards.cmd.sub.CreateSubCommand
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
+import org.bukkit.util.StringUtil
 
-class MainCommand : CommandExecutor {
+object MainCommand : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
-        return false
+        if (args[0].equals("create", true)) CreateSubCommand.handle(sender, args)
+        return true
+    }
+
+    override fun onTabComplete(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): MutableList<String> {
+        val completions = mutableListOf<String>()
+        val subCommands = mutableListOf("create")
+        StringUtil.copyPartialMatches(args[0], subCommands, completions)
+        completions.sort()
+        return completions
     }
 }
---
+++ src/main/kotlin/com/zp4rker/postcapturerewards/cmd/sub/CreateSubCommand.kt
@@ -0,0 +1,42 @@
+package com.zp4rker.postcapturerewards.cmd.sub
+
+import com.zp4rker.postcapturerewards.Post
+import com.zp4rker.postcapturerewards.permissionError
+import org.bukkit.ChatColor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+object CreateSubCommand {
+    private const val usage = "/capturepost create <radius>"
+
+    fun handle(sender: CommandSender, args: Array<out String>) {
+        // sender player check
+        if (sender !is Player) {
+            sender.sendMessage("${ChatColor.RED}You must be a player to run this command!")
+            return
+        }
+        // permission check
+        if (!sender.hasPermission("postcapturerewards.create")) {
+            sender.sendMessage(permissionError)
+            return
+        }
+        // arg exists check
+        if (args.size < 2) {
+            sender.sendMessage("${ChatColor.RED}Invalid arguments! You need to specify a radius. Usage: $usage")
+            return
+        }
+        // arg format check
+        val radius = args[1].toIntOrNull() ?: run {
+            sender.sendMessage("${ChatColor.RED}Please use a valid number. Usage: $usage")
+            return
+        }
+        // fetch location
+        val location = sender.location
+        // create post
+        Post(location, radius)
+        // send notification of creation
+        sender.sendMessage("${ChatColor.GREEN}Post successfully created!")
+        // instruct to add commands
+        sender.sendMessage("${ChatColor.YELLOW}To finish setting up the post, add the win and lose commands. Usage: /capturepost edit (win_commands|lose_commands) add <command>")
+    }
+}
